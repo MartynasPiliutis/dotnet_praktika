11d4337e7a3de8f86bd402e4935199c007bf3c4a
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-04-15 02:11:01 +0300

    task1 complete

---
+++ task1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "";
+            string pavarde = "";
+            Console.Write("Iveskite varda: ");
+            vardas = Console.ReadLine();
+            Console.Write("Iveskite pavarde: ");
+            pavarde = Console.ReadLine();
+            Console.WriteLine($"{vardas}, {pavarde}");
+            Console.ReadLine();
+        }
+    }
+}
