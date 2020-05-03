7a8e8c7d736466c4bae49b0e5bfed3fec510d477
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-05-03 05:01:54 +0300

    DiceRoll complete

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace DiceRoll
 {
@@ -10,6 +12,33 @@ namespace DiceRoll
     {
         static void Main(string[] args)
         {
+            Random rollDice = new Random();
+            int dice1;
+            int dice2;
+            string answer;
+
+            DiceRoll(rollDice, out dice1, out dice2, out answer);
+        }
+
+        private static void DiceRoll(Random rollDice, out int dice1, out int dice2, out string answer)
+        {
+            Console.Write("Press any key for dice roll...");
+            Console.ReadKey();
+            Console.Clear();
+
+            dice1 = rollDice.Next(1, 7);
+            dice2 = rollDice.Next(1, 7);
+
+            Console.WriteLine($"First dice: {dice1}");
+            Console.WriteLine($"Second dice: {dice2}");
+            Console.WriteLine("");
+            Console.Write("Again? y/n");
+            answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                Console.Clear();
+                DiceRoll(rollDice, out dice1, out dice2, out answer);
+            }
         }
     }
 }
