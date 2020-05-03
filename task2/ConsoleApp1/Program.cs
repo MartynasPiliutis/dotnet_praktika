6f88ac5e6078dba17069bf0c4aeb5d405f68898c
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-05-03 04:40:31 +0300

    DiceRoll

---
+++ task2/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb1;
+            int nb2;
+            Console.Write("Iveskite pirma skaiciu: ");
+            nb1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Iveskite antra skaiciu: ");
+            nb2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Skaiciu suma yra {nb1 + nb2}");
+            Console.ReadLine();
+        }
+    }
+}
